97d179e34d1ad3d585d1cccb45fe6752c3680f62
plutach <jananadi92@gmail.com>
2020-07-07 23:22:43 +0900

    test component

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="count">
+    <h1>{{msg}}</h1>
+    <button @click="onClick()">Click!</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Counter extends Vue {
+  @Prop() private msg: string = "Hey";
+
+  onClick(): void {
+    window.alert(this.msg);
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,12 @@
     <h1>Find your greens here</h1>
   </div>
 </template>
+<script>
+import Counter from "@/components/Counter.vue";
+export default {
+  name: "Counter",
+  components: {
+    Counter
+  }
+};
+</script>
