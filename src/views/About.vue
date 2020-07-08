f0b2cdf18526279d7fc34bffc99f66a2d07027bd
Jananadi Wedagedara <jananadi@onda.me>
2020-07-08 16:02:48 +0900

    Improvement-add-component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="about">
     <h1>Find your greens here</h1>
+    <Counter msg="Please select"/>
   </div>
 </template>
-<script>
-import Counter from "@/components/Counter.vue";
+<script lang='ts'>
+import Counter from '@/components/Counter.vue';
+
 export default {
-  name: "Counter",
+  name: 'Counter',
   components: {
-    Counter
-  }
+    Counter,
+  },
 };
 </script>
