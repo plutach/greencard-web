e6edf2763174d82b87d6a11c2f68d825bb0c5ec3
plutach <jananadi92@gmail.com>
2020-07-15 23:47:14 +0900

    test map component

---
+++ src/components/Map.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Map">
+    <h3>{{ map }}</h3>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Map extends Vue {
+  @Prop() private map!: string;
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>Find your greens here</h1>
+    <Map map="this is a map" />
   </div>
 </template>
+<script>
+import Map from "@/components/Map.vue";
+
+export default {
+  name: "About",
+  components: {
+    Map
+  }
+};
+</script>
